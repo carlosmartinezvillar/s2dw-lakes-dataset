2f811330ed964bc5b6a991364ef37c1409a8e0b1
cimv <cimcrd@umsystem.edu>
2026-07-27 13:36:00 -0700

    forgot to update optim in loop

--- models/train.py
+++ models/train.py
@@ -349,9 +349,11 @@ def train_and_validate(model,dataloaders,optimizer,loss_fn,scheduler,epochs=50,n
 
 			# BACKPROP
 			optimizer.zero_grad()
+			loss.backward()
 			# scaler.scale(loss).backward()
 			# scaler.unscale_(optimizer)
 			torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+			optimizer.step()
 			# scaler.step(optimizer)
 			# scaler.update()
 
